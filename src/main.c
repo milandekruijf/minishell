21dd57555e54910a00684044f89a110de3ae1c2e
Milan De Kruijf <mde-krui@f0r4s18.codam.nl>
2024-10-28 16:14:11 +0100

    removed uncessary line

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mde-krui <mde-krui@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/10/11 13:26:50 by mde-krui      #+#    #+#                 */
-/*   Updated: 2024/10/28 16:12:43 by mde-krui      ########   odam.nl         */
+/*   Updated: 2024/10/28 16:13:38 by mde-krui      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ int	main(int argc, const char **argv, const char **envp)
 {
 	t_env_var_list	*env_vars;
 
-	(void)argv;
 	listen_sigint();
 	env_vars = parse_envp(envp);
 	if (argc == 1)
